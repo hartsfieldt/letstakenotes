9e0a88814c76c8128cbc74fb527abcf49c4c7548
Your Name <you@example.com>
2022-10-04 11:39:08 -0400

    created requires and listening port

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const PORT = process.end.PORT || 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.static(
+    "public"
+))
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+})
