e6fbb65241b0046af4c4c1e446651051aa075aa7
Your Name <you@example.com>
2022-10-04 20:14:45 -0400

    updated folder structure and created html routes

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 const express = require('express');
-
-const PORT = process.end.PORT || 3001;
 const app = express();
 
-app.use(express.json());
+const PORT = process.env.PORT || 3001;
+
+const htmlRoutes = require("./routes/htmlRoutes")
+
 app.use(express.urlencoded({
     extended: true
 }));
+
+app.use(express.json());
+
 app.use(express.static(
     "public"
 ))
+
+app.use("/", htmlRoutes)
+
+
+
+// Routes
+// app.post
+// app.get - GET /api/notes - GET /notes
+//GET *
+//POST /api/notes
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
